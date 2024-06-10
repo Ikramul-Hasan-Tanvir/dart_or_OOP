dd815cc9badd97cb56e739bcc443e444c622ff5c
Ikramul Hasan Tanvir <ikramulhasantanvir@Ikramuls-MacBook-Air.local>
2024-06-11 03:13:04 +0600

    Initial commit

---
+++ basic_dart/hello_world.dart
@@ -0,0 +1,19 @@
+main(){
+
+  print('Good morning!'); // print String.
+  print(01927448628); //print int.
+  print(3.50); //print double.
+  print(true); //print bool.
+
+  // print(problem solve);
+  print(30+50);
+  print(30-50);
+  print(30*50);
+  print(30/50);
+  print(30%50);
+
+  int a = 5;
+  double b = 3.2;
+
+  print(a+b);
+}
---
+++ basic_dart/variables.dart
@@ -0,0 +1,32 @@
+main(){
+
+  // primitive data types.
+  // String (textual data), int (number data exm:123), double(number exm:12.00),bool(true Or false);
+
+// variable -> container -> contains value.
+// data-type variable-name = value.
+
+  String userName = 'Ikramul';
+  print(userName);
+  userName = 'Ikramul hasan';
+  print(userName);
+  userName = 'Ikramul hasan Tanvir';
+  print(userName);
+
+  int userNumber = 019274448628;
+  print(userNumber);
+  userNumber = 01831266537;
+  print(userNumber);
+
+  double cgpa = 3.50;
+  print(cgpa);
+  cgpa = 3.80;
+  print(cgpa);
+
+  bool isSuccess = true;
+  print(isSuccess);
+  isSuccess = false;
+  print(isSuccess);
+
+}
+
